9e46c54b141cbbb7f8d0f7630222a1b1a33d6a1c
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-23 18:15:27 -0400

    updated stringUtils

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -5,3 +5,12 @@ export const capitalize = (str) =>
   str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
 export const reverse = (str) => str.split("").reverse().join("");
+
+// Create a test to check the below isPalindrome
+// // A palindrome is a word, phrase, number, or other sequence of characters that reads the same
+// // forward and backward (ignoring spaces, punctuation, and capitalization
+
+export const isPalindrome = (str) => {
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  return cleanedStr === cleanedStr.split("").reverse().join("");
+};
